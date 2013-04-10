f1bafd6012c24150a473a43212b5205905b926da
Celso de Sa <celsodessa@gmail.com>
2013-04-10 02:03:06 -0300

    correct the error add validations and default image.

--- app/models/define.rb
+++ app/models/define.rb
@@ -2,7 +2,8 @@ class Define < ActiveRecord::Base
   attr_accessible :definition, :term, :image, :relevance, :user_id
   mount_uploader :image, ImageUploader
   belongs_to :user
-
+  validates :definition, :term, :image, :relevance, presence: true
+  validates :term, uniqueness: true
 
   	def self.text_search(query)
 	  if query.present?
