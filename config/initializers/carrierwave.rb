c83aa27d759b60429839f8d7bcde36c8ee4eb145
Celso de Sa <celso.sa.lopes@gmail.com>
2019-03-06 09:25:11 -0300

    AWS Config Vars properly set via Dashboard

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
 	config.fog_credentials = {
 		provider: "AWS",
-		aws_access_key_id: "AKIAJ47MUKW3LYIU45LA",
-		aws_secret_access_key: "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"
+		aws_access_key_id: KEY_ID,
+		aws_secret_access_key: SECRET_ACCESS_KEY
 	}
 	config.fog_directory = "railsplex"
 end
